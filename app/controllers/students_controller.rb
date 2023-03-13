7992ea0d4fda32ed2a1de3e8de309e3ea0a39624
Randy Kipkurui Cherutich <randy.kipkurui4@gmail.com>
2023-03-13 23:44:11 +0300

    Completed Assignment.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,18 @@
 class StudentsController < ApplicationController
 
   def index
+    # students = if params[:name]
+    #               Student.by_name(params[:name])
+    #             else
+    #               Student.all
+    #             end
     students = Student.all
     render json: students
+  end
+
+  def show
+    stu = Student.find(params[:id])
+    render json: stu
   end
 
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -4,4 +4,7 @@ class Student < ApplicationRecord
     "#{first_name} #{last_name}"
   end
 
+  # scope (https://www.rubyguides.com/2019/10/scopes-in-ruby-on-rails/)
+  # scope :by_name, ->(name) { where("first_name LIKE ? OR last_name LIKE ?", "%#{name}%", "%#{name}%") }
+
 end
