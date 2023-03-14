2b238cf4c048513e69e5e1db64b706d71ffc7c98
Randy Kipkurui Cherutich <randy.kipkurui4@gmail.com>
2023-03-14 16:55:37 +0300

    Add bonus solution: query params.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -6,7 +6,13 @@ class StudentsController < ApplicationController
     #             else
     #               Student.all
     #             end
-    students = Student.all
+    # students = Student.all
+
+    students = if params[:name]
+        Student.where(["first_name LIKE ? OR last_name LIKE ?", params[:name], params[:name]])
+      else
+        Student.all
+      end
     render json: students
   end
 
